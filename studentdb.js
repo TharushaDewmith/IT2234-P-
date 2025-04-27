ef5ea9830acafaf891c68f69861b2cd36e643c35
TharushaDewmith <tharuwadewmith123@gmail.com>
2025-04-27 13:10:05 +0530

    Add files via upload

---
+++ studentdb.js
@@ -0,0 +1,10 @@
+
+let students=[
+    {regno:'2021ICT118',name:'Tharusha',gender:'male',age:23,course:'IT'},
+    {regno:'2021ICT119',name:'John',gender:'male',age:23,course:'IT'},
+    {regno:'2021ICT120',name:'James',gender:'male',age:21,course:'Bio'},
+    {regno:'2021ICT121',name:'Alia',gender:'female',age:22,course:'Maths'},
+    {regno:'2021ICT122',name:'Prabashi',gender:'female',age:21,course:'IT'}
+    ];
+
+module.exports=students;
